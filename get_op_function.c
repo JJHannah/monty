a06c702bb92ccfcecb9063b294b3d4db5c809394
JJHannah <zemjefferyzem@gmail.com>
2022-12-15 10:32:08 -0800

    initial commit

---
+++ get_op_function.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * get_op_func - select the correct operation function asked by user
+ * @s: operator argument
+ * Return: function pointer corresponding to operator given
+ */
+
+void (*get_op_func(char *s))(stack_t**, unsigned int)
+{
+instruction_t ops[] = {
+{"push", op_push},
+{"pall", op_pall},
+{"pint", op_pint},
+{NULL, NULL}
+};
+int i = 0;
+
+while (ops[i].opcode != NULL && strcmp((ops[i].opcode), s))
+i++;
+return (ops[i].f);
+}
